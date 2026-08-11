76688541bd9054b053b45f84a78f0adbec9621bd
Devesh07-max <deveshkumawat077@gmail.com>
2026-08-11 07:29:56 +0530

    Adjust default values for download configuration
    Updated environment variable defaults for download settings.

--- config.py
+++ config.py
@@ -40,8 +40,8 @@ class PyroConf(object):
     SESSION_STRING = getenv("SESSION_STRING")
     BOT_START_TIME = time()
 
-    MAX_CONCURRENT_DOWNLOADS = int(getenv("MAX_CONCURRENT_DOWNLOADS", "1"))
-    BATCH_SIZE = int(getenv("BATCH_SIZE", "50"))
-    FLOOD_WAIT_DELAY = int(getenv("FLOOD_WAIT_DELAY", "10"))
+    MAX_CONCURRENT_DOWNLOADS = int(getenv("MAX_CONCURRENT_DOWNLOADS", "3"))
+    BATCH_SIZE = int(getenv("BATCH_SIZE", "100"))
+    FLOOD_WAIT_DELAY = int(getenv("FLOOD_WAIT_DELAY", "5"))
 
     FORWARD_CHAT_ID = getenv("FORWARD_CHAT_ID", "-1004419031099").strip() or None
